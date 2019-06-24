0e3afd7c544a4f8d9232e9f193a5ec51aedadddd
Ascendzor <wow.troymare@gmail.com>
2019-06-24 23:49:42 +1200

    well that wasn't so hard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,79 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = props => {
 
+  const [timeyWimeys, setTimeyWimeys] = useState([0, 0, 0]) //[hours, minutes, seconds]
+  const [angleShmangles, setAngleShmangles] = useState([0, 0, 0]) //[hourAngle, minuteAngle, secondAngle]
+  const [lines, setLines] = useState(<g />)
+
+  useEffect(() => {
+    setInterval(() => {
+      const now = new Date()
+      const milliseconds = now.getMilliseconds()
+      
+      const seconds = now.getSeconds() + (milliseconds/1000)
+      const minutes = now.getMinutes() + (seconds / 60)
+      const hours = now.getHours() + (minutes / 60)
+      const secondAngle = 360 * seconds / 60
+      const minuteAngle = 360 * minutes / 60
+      // const secondAngle = 360 * 5 / 60
+      // const minuteAngle = 360 * 0 / 60
+
+      // setAngleShmangles([
+      //   360 * hours / 12,
+      //   360 * minutes / 60,
+      //   360 * seconds / 60,
+      // ])
+
+      // setAngleShmangles([
+      //   0,
+      //   0,
+      //   360 * 5 / 60,
+      // ])
+
+      const lineLength = 75
+      const lineStyle = {strokeWidth: 1, stroke: 'grey'}
+      const fractate = (minuteAngle, secondAngle, iteration) => {
+        if(iteration == 10) return
+        return <g>
+          <g transform={`rotate(${minuteAngle})`}>
+            <path style={lineStyle} d={`M 0,0 L 0,${-lineLength/iteration} Z`} />
+            <g transform={`translate(0,${-lineLength/iteration})`}>
+              {fractate(minuteAngle, secondAngle, iteration+1)}
+            </g>
+          </g>
+          <g transform={`rotate(${secondAngle})`}>
+            <path style={lineStyle} d={`M 0,0 L 0,${-lineLength/iteration} Z`} />
+            <g transform={`translate(0, ${-lineLength/iteration})`}>
+              {fractate(minuteAngle, secondAngle, iteration+1)}
+            </g>
+          </g>
+        </g>
+      }
+
+      const lines = fractate(minuteAngle, secondAngle, 1)
+      // console.log(lines)
+      setLines(lines)
+      // <path transform={`rotate(${angleShmangles[1]})`} style={{stroke: 'grey', strokeWidth: 2}} d='M 0,0 L 0,-75 Z' />
+    }, 32)
+  }, [])
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
       <div style={{marginLeft: 'auto', marginRight: 'auto', height: '100%', width: '100%', position: 'absolute'}}>
-        <svg width='400' height='400' viewBox="0 0 400 400" style={{paddingTop: 50}}>
-          <g transform='translate(200, 200)'>
+        <svg width='600' height='600' viewBox="0 0 600 600" style={{paddingTop: 50}}>
+          <g transform='translate(300, 300)'>
             {/* <path style={{stroke: 'red', strokeWidth: 3}} d='M 0,-100 L 0,-120 Z' /> */}
             {[...Array(12)].map((nothing, i) => {
               const rotationFraction = i/12
               const rotationInDegrees = 360 * rotationFraction
-              return <g transform={`rotate(${rotationInDegrees})`}>
+              return <g transform={`rotate(${rotationInDegrees})`} key={i}>
                 <path style={{stroke: 'grey', strokeWidth: 2}} d='M 0,-100 L 0,-115 Z' />
                 <text transform={`translate(0,-85) rotate(-${rotationInDegrees})`} dominantBaseline="middle" textAnchor="middle" style={{stroke: 'grey'}}>{i || 12}</text>
               </g>
@@ -24,10 +81,13 @@ const App = props => {
             {[...Array(60)].map((nothing, i) => {
               const rotationFraction = i/60
               const rotationInDegrees = 360 * rotationFraction
-              return <g transform={`rotate(${rotationInDegrees})`}>
+              return <g transform={`rotate(${rotationInDegrees})`} key={i}>
                 <path style={{stroke: 'grey', strokeWidth: 1}} d='M 0,-110 L 0,-115 Z' />
               </g>
             })}
+            <g>
+              {lines}
+            </g>
           </g>
         </svg>
       </div>
